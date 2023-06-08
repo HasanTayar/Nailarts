e9485e8bbcaeab8a10e94a733db7d1398cd4ee67
HasanTayar <hasantayar1602@gmail.com>
2023-06-08 23:05:13 +0300

    Added the cors on server

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
+app.use(cors());
 
-// Other app configurations
 
-// Use the userRoutes router
 app.use('/user',userRoutes);
 
-// Start the server
+// Start the server on the local
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
