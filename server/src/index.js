fc231da7fbe3adb0b71b7f306d31a46259da73d4
HasanTayar <hasantayar1602@gmail.com>
2023-06-08 23:31:26 +0300

    Fixed the issue with cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
-app.use(cors());
+const whitelist = [process.env.REACT_APP_URL, 'http://localhost:4000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 
 app.use('/user',userRoutes);
