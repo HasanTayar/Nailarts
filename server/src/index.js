9340d6137e0760c4670d7e3e36dc6297f81b3e99
HasanTayar <hasantayar1602@gmail.com>
2023-06-09 11:58:33 +0300

    ...

--- server/src/index.js
+++ server/src/index.js
@@ -2,18 +2,7 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
-const whitelist = [process.env.REACT_APP_URL, 'http://localhost:4000'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions));
+app.use(cors("*"));
 
 
 app.use('/user',userRoutes);
